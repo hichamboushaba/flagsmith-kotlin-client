c3a0874dffb1a7d6a223b36b58325c2dbaae030f
hichamboushaba <hicham.boushaba@gmail.com>
2026-09-06 17:52:42 +0100

    Move to Kotlin 2.3.21 and the compilerOptions DSL

--- FlagsmithClient/build.gradle.kts
+++ FlagsmithClient/build.gradle.kts
@@ -4,6 +4,7 @@ import kotlinx.kover.api.VerificationTarget
 import kotlinx.kover.api.VerificationValueType
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.io.ByteArrayOutputStream
 import java.lang.System.getenv
 import java.util.*
@@ -40,10 +41,8 @@ kotlin {
     androidTarget {
         publishLibraryVariants("release", "debug")
 
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = JavaVersion.VERSION_11.toString()
-            }
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
         }
     }
     jvm()
